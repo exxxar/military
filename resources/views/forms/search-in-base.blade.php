88c8c2ac0de21b7c1a709941026ec94cef8ca5bc
Mona Lisa <exxxar@gmail.com>
2022-04-04 02:52:53 +0300

    update

--- resources/views/forms/search-in-base.blade.php
+++ resources/views/forms/search-in-base.blade.php
@@ -3,7 +3,7 @@
 @section("content")
 
     @isset($_GET["uid"])
-        <search-in-base-form-component :user-id="{{$_GET["uid"]??null}}"></search-in-base-form-component>
+        <search-in-base-form-component user-id="{{$_GET["uid"]??null}}"></search-in-base-form-component>
     @else
         <search-in-base-form-component></search-in-base-form-component>
     @endisset
