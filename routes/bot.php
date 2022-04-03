0a57d2a46c5fc07e07098c582a41b8e3442ad36a
Mona Lisa <exxxar@gmail.com>
2022-04-03 23:10:44 +0300

    large update

--- routes/bot.php
+++ routes/bot.php
@@ -373,13 +373,13 @@ MilitaryServiceFacade::bot()
         if (count($photos) == 0)
             MilitaryServiceFacade::bot()->replyKeyboard($message, $keyboard);
 
-        if (count($photos) == 1)
+        if (count($photos) >= 1)
             MilitaryServiceFacade::bot()->replyPhoto($message,
                 "https://shelte-dpr.ru/people-photo/" . $photos[0],
                 $keyboard
             );
 
-        if (count($photos) > 1) {
+    /*    if (count($photos) > 1) {
 
             $media = [];
 
@@ -394,10 +394,10 @@ MilitaryServiceFacade::bot()
 
             MilitaryServiceFacade::bot()
                 ->sendMediaGroup($user_id,
-                    json_encode($media)
+                    $media
                 )->replyKeyboard($message, $keyboard);
 
-        }
+        }*/
 
         switch ($type) {
             case 0:
@@ -490,10 +490,13 @@ MilitaryServiceFacade::bot()
 
                 $hAids = $hAids->take(30);
 
+
+
                 foreach ($hAids as $index => $item) {
+                    $bId = base64_encode($item->id);
+
                     $tmp .= ($index + 1) . "# " . $item->full_name . " (гум. помощь "
-                        . \Carbon\Carbon::parse($item->issue_at)->toDateString() . ") <a href='https://shelter-dpr.ru/send-message?id=" .
-                        base64_encode($item->id) . "'>\xF0\x9F\x93\xA7Оставить записку</a>\n";
+                        . \Carbon\Carbon::parse($item->issue_at)->toDateString() . ") <a href='https://shelter-dpr.ru/forms/send-message?id=$bId'>\xF0\x9F\x93\xA7Оставить записку</a>\n";
                 }
 
                 MilitaryServiceFacade::bot()->reply(
