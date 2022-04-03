9af615a4b764f91d36d4aa2fe2cbb380bd90305c
Mona Lisa <exxxar@gmail.com>
2022-04-04 01:34:13 +0300

    update

--- resources/views/forms/send-message.blade.php
+++ resources/views/forms/send-message.blade.php
@@ -4,7 +4,7 @@
 
 
     @if(!is_null($id))
-        <send-message-form-component :user-id="{{$id}}" data-type="{{$_GET["by"]}}"></send-message-form-component>
+        <send-message-form-component :user-id="{{$id}}" data-type="{{$_GET["by"]??"haids"}}"></send-message-form-component>
     @else
         <send-message-form-component></send-message-form-component>
     @endif
